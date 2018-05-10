1aa805bdea7b330e81cc32d534c98cbd2cded1be
souhain <edgarparedes283@gmail.com>
2018-05-10 15:24:38 -0500

    perfecto

---
+++ src/main/java/com/example/demo/Calculadora.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Calculadora {
+	 
+		public static int suma(int a, int b){
+			
+			return a+b;
+		}
+		
+		
+		public static int resta(int a, int b){
+			return a-b;
+		}
+
+}
---
+++ src/test/java/com/example/demo/CalculadoraTest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+	
+	@Test
+	public void testsuma() {
+		
+	int	resultado = Calculadora.suma(5,6); 
+	int esperado = 10;
+	
+	assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testresta() {
+		
+	int	resultado = Calculadora.resta(5,6); 
+	int esperado = -1;
+	
+	assertEquals(esperado, resultado);
+	}
+
+}
